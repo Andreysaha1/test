f74d20a64b5b984d7623b9e8a950fbf99eb7ec01
Andreysaha1 <165813745+Andreysaha1@users.noreply.github.com>
2024-09-09 22:08:04 +0300

    mod.js

--- mod.js
+++ mod.js
@@ -5,3 +5,8 @@ const ModalHandler = FAPI.imodules.ModalHandler;
 const ChunkUpdates = FAPI.routes.ChunkUpdates;
 
 const mod = FAPI.registerMod('test');
+
+// region arrow
+const arrow = mod.registerArrow(0);
+arrow.name = ['arrow'];
+arrow.icon_url = "https://logic-arrows.io/res/sprites/arrow1.png?v=1_2_1";

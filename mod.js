0f5d70a6dcd18ce41add63c9e5ccf803fad3d387
Andreysaha1 <165813745+Andreysaha1@users.noreply.github.com>
2024-09-09 22:56:54 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -11,5 +11,5 @@ const arrow = mod.registerArrow(0);
 arrow.name = ['arrow'];
 arrow.icon_url = "https://raw.githubusercontent.com/Andreysaha1/test/main/arrow19%20(2).png";
 arrow.transmit = (arrow) => {
-    let [color, activation, transmit] = arrow.custom_data;
-    if (arrow.signal !== 0 && transmit === 1) ChunkUpdates.updateCount(arrow, ChunkUpdates.getArrowAt(arrow.chunk, arrow.x, arrow.y, arrow.rotation, arrow.flipped, -1, 0));
+    if (arrow.signal === 1) ChunkUpdates.updateCount(arrow, ChunkUpdates.sgetArrowAt(arrow, -1, 0))
+}

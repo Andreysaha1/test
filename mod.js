5bfa15e32c2cd6c79a8e6d49cfd87c9eaaa0cae4
Andreysaha1 <165813745+Andreysaha1@users.noreply.github.com>
2024-09-09 22:43:43 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -10,3 +10,5 @@ const mod = FAPI.registerMod('test');
 const arrow = mod.registerArrow(0);
 arrow.name = ['arrow'];
 arrow.icon_url = "https://raw.githubusercontent.com/Andreysaha1/test/main/arrow19%20(2).png";
+if (arrow.signalsCount > 0) arrow.signal = 6;
+    else arrow.signal = 0;

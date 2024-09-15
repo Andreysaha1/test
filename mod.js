a9db2187528c0fac9dc0cfbf232c3d03cb213dd0
Andreysaha1 <165813745+Andreysaha1@users.noreply.github.com>
2024-09-15 15:00:23 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -9,7 +9,4 @@ const mod = FAPI.registerMod('test');
 // region arrow
 const arrow = mod.registerArrow(0);
 arrow.name = ['arrow'];
-arrow.icon_url = "https://raw.githubusercontent.com/Andreysaha1/test/main/arrow19%20(2).png";
-arrow.transmit = (arrow) => {
-    if (arrow.signal === 1) ChunkUpdates.updateCount(arrow, ChunkUpdates.sgetArrowAt(arrow, -1, 0))
-} ;
+arrow.icon_url = "https://raw.githubusercontent.com/Andreysaha1/test/main/images.jfif";
